8e3b530acca7dc8f8f215df26a1e5886c71f67aa
Jini <mjindev@gmail.com>
2022-07-08 19:18:12 +0900

    data, domain 모듈 변경

--- data/src/androidTest/java/com/mjini/data/ExampleInstrumentedTest.kt
+++ data/src/androidTest/java/com/mjini/data/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.mjini.data", appContext.packageName)
+        assertEquals("com.mjini.data.test", appContext.packageName)
     }
 }
